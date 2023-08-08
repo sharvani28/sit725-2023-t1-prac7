5ccdd8af38e8a9c37a9dccd67bbe8859addf640d
sharvani28 <sharvanikandala@gmail.com>
2023-08-08 19:34:36 +1000

    minimal requirements
    Adding a database

--- script.js
+++ script.js
@@ -1,46 +1,56 @@
-const cardList = [
-    {
-        title: "Kitten 2",
-        image: "https://i.pinimg.com/originals/3b/93/57/3b93578853ccc4a90ffe794a1962ba6d.jpg",
-        link: "About Kitten 2",
-        desciption: "Demo desciption about kitten 2"
-    },
-    {
-        title: "Kitten 3",
-        image: "https://i.pinimg.com/474x/72/e4/ad/72e4adae2dd6b532bd2d2c28f7c3c86b--midget-cat-dwarf-cat.jpg",
-        link: "About Kitten 3",
-        desciption: "Demo desciption about kitten 3"
-    }
-]
-const clickMe = () => {
-    alert("Thanks for clicking me. Hope you have a nice day!")
-}
-const submitForm = () => {
-    let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
-    console.log("Form Data Submitted: ", formData);
-}
+
+
 const addCards = (items) => {
     items.forEach(item => {
         let itemToAppend = '<div class="col s4 center-align">' +
-            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.path + '">' +
             '</div><div class="card-content">' +
-            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.path + '</a></p></div>' +
             '<div class="card-reveal">' +
-            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
-            '<p class="card-text">' + item.desciption + '</p>' +
+            '<span class="card-title grey-text text-darken-4">' + item.subtitle + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' + item.description + '</p>' +
             '</div></div></div>';
         $("#card-section").append(itemToAppend)
     });
 }
+const submitForm = () => {
+    let formData = {};
+    formData.title = $('#title').val();
+    formData.subtitle = $('#subtitle').val();
+    formData.path = $('#path').val();
+    formData.description = $('#description').val();
+    console.log("Form Data Submitted: ", formData);
+    postCat(formData);
+}
+
+function postCat(cat){
+    $.ajax({
+        url:'/api/cat',
+        type:'POST',
+        data:cat,
+        success: (result)=>{
+            if(result.statusCode == 201){
+                alert('cat post successful');
+            }
+        }
+    });
+}
+
+function getAllCats(){
+    $.get('/api/cats', (res)=>{
+        if(res.statusCode === 200){
+            addCards(res.data);
+        }
+        
+    });
+}
+
 $(document).ready(function () {
     $('.materialboxed').materialbox();
     $('#formSubmit').click(() => {
         submitForm();
     })
-    addCards(cardList);
+    
     $('.modal').modal();
+    getAllCats();
 });
--- server.js
+++ server.js
@@ -1,10 +1,67 @@
 let express = require('express');
 let app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sharvanikandala:Sharvani28@cluster0.gnhkbv7.mongodb.net/?retryWrites=true&w=majority";
 let port = process.env.port || 3000;
+let collection;
+
 app.use(express.static(__dirname + '/'));
-app.get('/', (req, res)=>{
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function runDBConnection() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    collection= client.db().collection('Cat');
+    console.log(collection);
+  } catch(ex){
+    console.error(ex);
+  }
+}
+
+
+
+app.get('/', function(req, res){
     res.render('index.html');
 });
+
+app.get('/api/cats', (req,res) => {
+    getAllCats((err,result)=>{
+        if(!err){
+            res.json({statusCode:200, data:result, message:'get all cats success'});
+        }
+    });
+});
+
+app.post('/api/cat', (req,res)=>{
+    let cat= req.body;
+    postCat(cat, (err, result)=>{
+        if(!err){
+            res.json({statusCode:201, data:result, message:'success'});
+        }
+    });
+});
+
+function postCat(cat, callback){
+    collection.insertOne(cat, callback);
+}
+function getAllCats(callback){
+    collection.find({}).toArray(callback);
+}
 
 app.get('/addTwoNumbers', (req,res)=>{
     let num1= req.query.number1;
@@ -17,4 +74,6 @@ app.get('/addTwoNumbers', (req,res)=>{
 
 app.listen(port, ()=> {
     console.log('server started - 2');
+    runDBConnection()
+    
 });
